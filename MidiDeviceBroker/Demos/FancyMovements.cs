c0dd4a1c886055d3c725da76830de52743be53ab
Equinox <info@importantpeople.nl>
2023-04-28 17:39:27 +0200

    Seperated Fancy Movement Demo into its own class

---
+++ MidiDeviceBroker/Demos/FancyMovements.cs
@@ -0,0 +1,39 @@
+﻿using MidiDeviceBroker.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidiDeviceBroker.Demos
+{
+    internal class FancyMovements
+    {
+        public static void Start(IDevice device)
+        {
+            // Do some fancy movements
+            Random random = new Random();
+            for (int i = 0; i < 200; i++)
+            {
+                // Set a slider
+                var randomValueFader = random.Next(0, 128);
+                var randomFader = device.Controls.Faders[random.Next(device.Controls.Faders.Count)];
+                device.SetControlToValue(randomFader, randomValueFader);
+
+                // Set a button
+                var randomValueButton = random.Next(2);
+                if (randomValueButton == 1) randomValueButton = 127;
+                var randomButton = device.Controls.Buttons[random.Next(device.Controls.Buttons.Count)];
+                device.SetControlToValue(randomButton, randomValueButton);
+                // Give the device a little time to actually do this
+                Thread.Sleep(100);
+            }
+
+            // Reset all the faders to 0
+            foreach (var fader in device.Controls.Faders) device.SetControlToValue(fader, 0);
+
+            // Reset all the buttons to off
+            foreach (var button in device.Controls.Buttons) device.SetControlToValue(button, 0);
+        }
+    }
+}
--- MidiDeviceBroker/Program.cs
+++ MidiDeviceBroker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MidiDeviceBroker.Devices;
+using MidiDeviceBroker.Demos;
 using NAudio.Midi;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
@@ -21,7 +22,7 @@ if (int.TryParse(Console.ReadLine(), out int x))
     switch (x)
     {
         case 1:
-            FancyMovements(device);
+            FancyMovements.Start(device);
             break;
         default:
             break;
@@ -30,32 +31,6 @@ if (int.TryParse(Console.ReadLine(), out int x))
 else
 {
     Console.WriteLine("Input not recognised, exiting program...");
-}
-
-void FancyMovements(IDevice device) {
-    // Do some fancy movements
-    Random random = new Random();
-    for (int i = 0; i < 200; i++)
-    {
-        // Set a slider
-        var randomValueFader = random.Next(0, 128);
-        var randomFader = device.Controls.Faders[random.Next(device.Controls.Faders.Count)];
-        device.SetControlToValue(randomFader, randomValueFader);
-
-        // Set a button
-        var randomValueButton = random.Next(2);
-        if (randomValueButton == 1) randomValueButton = 127;
-        var randomButton = device.Controls.Buttons[random.Next(device.Controls.Buttons.Count)];
-        device.SetControlToValue(randomButton, randomValueButton);
-        // Give the device a little time to actually do this
-        Thread.Sleep(100);
-    }
-
-    // Reset all the faders to 0
-    foreach (var fader in device.Controls.Faders) device.SetControlToValue(fader, 0);
-
-    // Reset all the buttons to off
-    foreach (var button in device.Controls.Buttons) device.SetControlToValue(button, 0);
 }
 
 // Wait for user to be done
